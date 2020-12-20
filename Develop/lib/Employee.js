fcd51443337fc5582200cebe7f0c8bdb4b21965e
Brandon <brandonbgrayson@gmail.com>
2020-12-20 18:26:11 -0500

    refactored Empolyee.js to include classes/ getgihub test passed

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,20 +1,22 @@
 // TODO: Write code to define and export the Employee class
 // we need a function called getName()
-function Employee (name, id, email) {
-    this.name = name
-    this.id = id
-    this.email = email
-    this.getName = function (name) {
-        return this.name
-    }
-    this.getId = function (id) {
-        return this.id
-    }
-    this.getEmail = function (email) {
-        return this.email
-    }
-    this.getRole = function (role) {
-        return 'Employee'
+class Employee {
+    constructor (name, id, email) {
+        this.name = name
+        this.id = id
+        this.email = email
+        this.getName = function (name) {
+            return this.name
+        }
+        this.getId = function (id) {
+            return this.id
+        }
+        this.getEmail = function (email) {
+            return this.email
+        }
+        this.getRole = function (role) {
+            return 'Employee'
+        }
     }
 }
 
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,7 +1,16 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 const Employee = require ("./Employee")
 
-class Engineer extends Employee (name, id, email, github) {
-
+class Engineer extends Employee {
+        github(githubuser) {
+            return 'GitHubUser'
+        }
+        getRole (role) {
+            return 'Engineer'
+        }
+        getGithub(github) {
+            return 'GitHubUser'
+        }
+    
 }
 module.exports = Engineer
